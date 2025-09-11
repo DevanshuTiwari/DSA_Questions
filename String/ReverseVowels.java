7eb3128c678f5ff0b78cd893315c141e2d129e39
DevanshuTiwari <devanshu1336@gmail.com>
2025-09-11 17:20:02 +0530

    string questions

---
+++ String/ReverseVowels.java
@@ -0,0 +1,45 @@
+package String;
+
+public class ReverseVowels {
+    public static void main(String[] args) {
+        String s = "IceCreAm";
+        String ans = reverseVowel(s);
+
+        System.out.println(ans);
+    }
+
+    private static String reverseVowel(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+
+        int i = 0, j = s.length() - 1;
+        StringBuilder str = new StringBuilder(s);
+
+        while (i < j) {
+            if (!isVowel(s.charAt(i))) {
+                i++;
+            }
+
+            if (!isVowel(s.charAt(j))) {
+                j--;
+            }
+
+            if (isVowel(s.charAt(i)) && isVowel(s.charAt(j))) {
+                char temp = str.charAt(i);
+                str.setCharAt(i, str.charAt(j));
+                str.setCharAt(j, temp);
+
+                i++;
+                j--;
+
+            }
+        }
+
+        return str.toString();
+    }
+
+    private static boolean isVowel(char c) {
+        return "aeiouAEIOU".indexOf(c) != -1;
+    }
+}
---
+++ String/SortVowels.java
@@ -0,0 +1,66 @@
+package String;
+
+
+public class SortVowels {
+    public static void main(String[] args) {
+        String s = "lEetcOde";
+
+        String ans = sortVowels(s);
+        System.out.println(ans);
+    }
+
+    public static String sortVowels(String s) {
+        int[] vowelCount = new int[128];
+
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                vowelCount[c]++;
+            }
+        }
+
+        int vowelIndex = 0;
+        StringBuilder str = new StringBuilder(s);
+        String sortedVowelChars = "AEIOUaeiou";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (isVowel(s.charAt(i))) {
+
+                while (vowelCount[sortedVowelChars.charAt(vowelIndex)] == 0) {
+                    vowelIndex++;
+                }
+
+                str.setCharAt(i, sortedVowelChars.charAt(vowelIndex));
+                vowelCount[sortedVowelChars.charAt(vowelIndex)]--;
+            }
+        }
+
+        return str.toString();
+// ------------------------------------------------------------------------------------------------------------------------------------
+
+        // List<Character> vowels = new ArrayList<>();
+
+        // for(char c : s.toCharArray()){
+        //     if(isVowel(c)){
+        //         vowels.add(c);
+        //     }
+        // }
+
+        // Collections.sort(vowels);
+
+        // int vowelIndex = 0;
+        // StringBuilder str = new StringBuilder(s);
+        // for(int i  = 0; i < s.length(); i++){
+        //     if(isVowel(s.charAt(i))){
+        //         str.setCharAt(i,vowels.get(vowelIndex));
+        //         vowelIndex++;
+        //     }
+        // }
+
+        // return str.toString();
+
+    }
+
+    private static boolean isVowel(char c) {
+        return "aeiouAEIOU".indexOf(c) != -1;
+    }
+}
