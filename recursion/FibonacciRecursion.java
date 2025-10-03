17a3e90f8c749e08c22bf351269815e67cd835f2
DevanshuTiwari <devanshu1336@gmail.com>
2025-10-03 11:58:18 +0530

    recursion question

---
+++ recursion/FibonacciRecursion.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class FibonacciRecursion {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(fibo(n));
+    }
+
+    private static int fibo(int n) {
+        if (n == 0) return 0;
+
+        if (n == 1) return 1;
+
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
