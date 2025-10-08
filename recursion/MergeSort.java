2b891c2b0935e86f36bfa535741224966488552c
DevanshuTiwari <devanshu1336@gmail.com>
2025-10-08 15:47:17 +0530

    recursion question

---
+++ recursion/MergeSort.java
@@ -0,0 +1,57 @@
+package recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] input = {4, 6, 1, 2, 5, 3};
+
+        mergeSort(input, 0, input.length - 1);
+
+        System.out.println(Arrays.toString(input));
+
+    }
+
+    private static void mergeSort(int[] arr, int low, int high) {
+        if (low >= high) return;
+
+        int mid = (low + high) / 2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    private static void merge(int[] arr, int low, int mid, int high) {
+        int left = low;
+        int right = mid + 1;
+
+        List<Integer> list = new ArrayList<>();
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                list.add(arr[left]);
+                left++;
+            } else {
+                list.add(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            list.add(arr[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            list.add(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = list.get(i - low);
+        }
+    }
+}
